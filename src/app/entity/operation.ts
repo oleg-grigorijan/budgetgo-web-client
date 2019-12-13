7c3901bd18e2d2903199b4af8e93d3a6bdcf0e1f
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-12-13 23:56:38 +0300

    Operation entity is added

---
+++ src/app/entity/operation.ts
@@ -0,0 +1,14 @@
+import {Category} from './category';
+import {User} from './user';
+
+export class Operation {
+    id: number;
+    moneyDelta: number;
+    category: Category;
+    date: string;
+    description: string;
+    dateCreated: string;
+    creator: User;
+    dateModified: string;
+    lastEditor: User;
+}
