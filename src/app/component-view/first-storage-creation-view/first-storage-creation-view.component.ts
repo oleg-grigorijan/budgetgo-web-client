d51d3a5df2396412bae4806b323222431c95126b
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-12-21 16:44:41 +0300

    Fix: missed observable unsubscription

--- src/app/component-view/first-storage-creation-view/first-storage-creation-view.component.ts
+++ src/app/component-view/first-storage-creation-view/first-storage-creation-view.component.ts
@@ -1,18 +1,21 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {UserDetailsService} from '../../service/user-details.service';
 import {StoragesService} from '../../service/storages.service';
 import {Router} from '@angular/router';
 import {filter} from 'rxjs/operators';
 import {HomeViewComponent} from '../home-view/home-view.component';
+import {Subscription} from 'rxjs';
 
 @Component({
     selector: 'app-first-storage-creation-view',
     templateUrl: './first-storage-creation-view.component.html',
     styleUrls: []
 })
-export class FirstStorageCreationViewComponent implements OnInit {
+export class FirstStorageCreationViewComponent implements OnInit, OnDestroy {
 
     static readonly PATH = 'storages/create-first';
+
+    private subscription: Subscription;
 
     isReady = false;
 
@@ -24,12 +27,16 @@ export class FirstStorageCreationViewComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.storagesService.storages$.pipe(filter(s => s !== null)).subscribe(storages => {
+        this.subscription = this.storagesService.storages$.pipe(filter(s => s !== null)).subscribe(storages => {
             if (storages.length > 0) {
                 this.router.navigate([HomeViewComponent.PATH]);
             } else {
                 this.isReady = true;
             }
         });
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
     }
 }
